910090de2e15cbfd3f4b92c9c97bb9c3dbe2cce8
Oetoekkae <32675138+Oetoekkae@users.noreply.github.com>
2018-04-27 15:06:48 +0300

    Tietokoneen valinta

--- TestiLabra/Javascript.js
+++ TestiLabra/Javascript.js
@@ -2,6 +2,17 @@ var choices = ["Rock", "Paper", "Scissors"];
 function hmm (){
 	alert("Welcome! Welcome! You have arrived to the greatest game: 'Rock, paper or scissors?'");
 }
+function aiChs() {
+	var rndNum = Math.random()*2;
+	rndNum = Math.round(rndNum);
+	var chs = choices[rndNum];
+	$("#buttonDiv").append(
+	'<button class="button" id="aiChoice">chs</button>'),
+		$("#aiChoice").text(chs),
+		$("#aiChoice").animate({left: '300px'}, 'fast'),
+		$("#aiChoice").animate({top: '200px'}, 'fast');
+	
+}
 $(document).ready(function(){
 	var chsRock = "";
 	var chsPaper = "";
@@ -20,6 +31,9 @@ $(document).ready(function(){
 		$("#paper").hide('slow'),
 		$("#rock").animate({top: '200px'}, 'fast'),
 		$("#rock").animate({left: '200px'}, 'fast')
+		aiChs();
+		
+
 	});
 });
 $(document).ready(function(){
@@ -28,6 +42,7 @@ $(document).ready(function(){
 		$("#paper").hide('slow'),
 		$("#scissors").animate({top: '200px'}, 'fast'),
 		$("#scissors").animate({left: '200px'}, 'fast')
+		aiChs();
 	})
 })
 $(document).ready(function(){
@@ -36,6 +51,8 @@ $(document).ready(function(){
 		$("#rock").hide('slow'),
 		$("#paper").animate({top: '200px'}, 'fast'),
 		$("#paper").animate({left: '200px'}, 'fast')
+		aiChs();
 	})
 })
+
 
