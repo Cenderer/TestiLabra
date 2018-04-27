2f932e54f0271d855337172e5658382282020d93
Oetoekkae <32675138+Oetoekkae@users.noreply.github.com>
2018-04-27 12:44:16 +0300

    Valintoja v2.0

--- TestiLabra/Javascript.js
+++ TestiLabra/Javascript.js
@@ -1,4 +1,14 @@
+var choices = ["Rock", "Paper", "Scissors"];
 function hmm (){
 	alert("Hello World!");
+}
+function rockFunction() {
+	alert("You chose rock");
+}
+function scissorsFunction() {
+	alert("You chose scissors");
+}
+function paperFunction() {
+	alert("You chose paper");
 }
 hmm();
