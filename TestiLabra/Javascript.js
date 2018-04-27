f15064c1d8a3820a3fb6941414f9ecfb2417b043
Oetoekkae <32675138+Oetoekkae@users.noreply.github.com>
2018-04-27 13:40:03 +0300

    Lisää juttuja

--- TestiLabra/Javascript.js
+++ TestiLabra/Javascript.js
@@ -2,13 +2,19 @@ var choices = ["Rock", "Paper", "Scissors"];
 function hmm (){
 	alert("Hello World!");
 }
-function rockFunction() {
-	alert("You chose rock");
-}
-function scissorsFunction() {
-	alert("You chose scissors");
-}
-function paperFunction() {
-	alert("You chose paper");
-}
+$(document).ready(function(){
+	$("#rock").click(function(){
+		alert("You chose rock");
+	});
+});
+$(document).ready(function(){
+	$("#Scissors").click(function(){
+		alert("You chose scissors");
+	})
+})
+$(document).ready(function(){
+	$("#Paper").click(function(){
+		alert("You chose paper");
+	})
+})
 hmm();
