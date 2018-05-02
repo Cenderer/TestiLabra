67b76c69c7ea35751f64edc7f4b11559780f4670
Cenderer <32675164+Cenderer@users.noreply.github.com>
2018-05-02 11:14:38 +0300

    Added new game option
    Added new game option: Hangman and change the context to game center

--- TestiLabra/Javascript.js
+++ TestiLabra/Javascript.js
@@ -1,9 +1,39 @@
 var choices = ["Rock", "Paper", "Scissors"];
 var chs;
 var plrChs;
+
+$(document).ready(function(){
+		var chsRock = "";
+		var chsPaper = "";
+		var chsScissors = "";
+		hmm();	
+});
+
 function hmm (){
-	alert("Welcome! Welcome! You have arrived to the greatest game: 'Rock, paper or scissors?'");
+	alert("Welcome! You have arrived to the greatest gamecenter! " );
+	menu();
 }
+
+function menu(){
+	$("#gmeTitle").text("Choose a game");
+	$(".button").remove();
+	$("#buttonDiv").append(
+	    '<button class="button" id="rps">Rock, Paper and Scissors!</button>',
+		'<button class="button" id="hangMan">Hangman!</button>'
+	);
+	$("#rps").click(function(){
+		R_P_S();
+	});
+	$("#hangMan").click(function(){
+		hangManGme();
+	});
+};
+
+function hangManGme(){
+	
+	
+};
+
 function cmpr(plrChs, chs){
 	
 	switch(plrChs){
@@ -61,68 +91,81 @@ function aiChs() {
 	rndNum = Math.round(rndNum);
 	chs = choices[rndNum];
 	$("#buttonDiv").append(
-	'<button class="button" id="aiChoice">chs</button>'),
-		$("#aiChoice").text(chs),
-		$("#aiChoice").animate({left: '300px'}, 'fast'),
-		$("#aiChoice").animate({top: '200px'}, 'fast', function(){
+		'<button class="button" id="aiChoice"></button>'),
+		$("#aiChoice").animate({left: '300px'}, 1000),
+		$("#aiChoice").animate({top: '200px'}, 1000, function(){
+			$("#aiChoice").text(chs)
+		}),
+		$("#aiChoice").animate({top: '200px'}, "fast", function(){
 			chs = rndNum;
-			cmpr(plrChs, chs);
+			cmpr(plrChs, chs)
 		});
 }
 	
 function again(){
 	
-	$(".button").hide("slow", function(){
-		$("#buttonDiv").append(
+	$(".button").remove();
+	$("#buttonDiv").append(
 	    '<button class="button" id="yes">Yes, again!</button>',
-		'<button class="button" id="nope">No, not again</button>'
+		'<button class="button" id="nope">No, not again</button>',
+		'<button class="button" id="change">Change game</button>'
 		);
+	$("#yes").click(function(){
+		
+		R_P_S();
+		
+	});	
+	$("#nope").click(function(){
+		/*alert("There is no escape!");
+		start();*/
+		window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ","_blank", "width=500, height=500");
+	});
+	$("#change").click(function(){
+		menu();
 	});
 }
 
-$(document).ready(function start(){
-	var chsRock = "";
-	var chsPaper = "";
-	var chsScissors = "";
-	
+function R_P_S(){
+	$("#gmeTitle").text("Rock, Paper, Scissors");
+	$(".button").remove();
 	$("#buttonDiv").append(
-	    '<button class="button" id="rock">Rock</button>',
-	    '<button class="button" id="scissors">Scissors</button>',
-		'<button class="button" id="paper">Paper</button>'
+			'<button class="button" id="rock">Rock</button>',
+			'<button class="button" id="scissors">Scissors</button>',
+			'<button class="button" id="paper">Paper</button>'
 		
 		);
-	
-	hmm();
-	$("#rock").click(function(){
-		plrChs = "rock";
-		$("#scissors").hide('slow'),
-		$("#paper").hide('slow'),
-		$("#rock").animate({top: '200px'}, 'fast'),
-		$("#rock").animate({left: '200px'}, 'fast')
-		aiChs();
-		
+		$("#rock").click(function(){
+			plrChs = "rock";
+			$("#scissors").hide('fast'),
+			$("#paper").hide('fast'),
+			$("#rock").animate({top: '200px'}, 'fast'),
+			$("#rock").animate({left: '200px'}, 'fast')
+			aiChs();
+				
 
-	});
+		});
 
 
-	$("#scissors").click(function(){
-		plrChs = "scissors";
-		$("#rock").hide('slow'),
-		$("#paper").hide('slow'),
-		$("#scissors").animate({top: '200px'}, 'fast'),
-		$("#scissors").animate({left: '200px'}, 'fast')
-		aiChs();
-	});
+		$("#scissors").click(function(){
+			plrChs = "scissors";
+			$("#rock").hide('fast'),
+			$("#paper").hide('fast'),
+			$("#scissors").animate({top: '200px'}, 'fast'),
+			$("#scissors").animate({left: '200px'}, 'fast')
+			aiChs();
+		});
 
 
-	$("#paper").click(function(){
-		plrChs = "paper";
-		$("#scissors").hide('slow'),
-		$("#rock").hide('slow'),
-		$("#paper").animate({top: '200px'}, 'fast'),
-		$("#paper").animate({left: '200px'}, 'fast')
-		aiChs();
-	});
-});
+		$("#paper").click(function(){
+			plrChs = "paper";
+			$("#scissors").hide('fast'),
+			$("#rock").hide('fast'),
+			$("#paper").animate({top: '200px'}, 'fast'),
+			$("#paper").animate({left: '200px'}, 'fast')
+			aiChs();
+			});
+};
+
+
 
 
